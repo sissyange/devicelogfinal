2325cbdb2ad1a55f00ee8502217c0a27526562d9
gwapitasissy <palayangeline@gmail.com>
2026-09-25 14:44:51 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,10 @@
+/* =========================================================
+   DEVICELOG - SPLASH SCREEN
+   ========================================================= */
+
+// Wait until the page is completely loaded
+document.addEventListener("DOMContentLoaded", function () {
+
+    console.log("DeviceLog Splash Screen Loaded.");
+
+});
